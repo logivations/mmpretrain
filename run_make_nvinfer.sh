67e6b4169728a138230161978d5a29ab1ca7815b
VolodymyrRatushnyi <volodymyr.ratushnyi@logivations.com>
2025-04-17 23:07:25 +0300

    RTDT-3453 Label Studio: fix configs for models

--- run_make_nvinfer.sh
+++ run_make_nvinfer.sh
@@ -8,30 +8,15 @@ CLASSES=( "$@" )
 CLASSES=$(IFS=';' ; echo "${CLASSES[*]}")
 
 echo "[property]
-gpu-id=0
-
-# preprocessing parameters.
-net-scale-factor=0.01742919389
-offsets=123.675;116.128;103.53
-model-color-format=0
-scaling-filter=1 # 0=Nearest, 1=Bilinear
-
 onnx-file=state_classifier.onnx
 model-engine-file=state_classifier.onnx_b1_gpu0_fp16.engine
 
 # model config
 infer-dims=3;128;128
-batch-size=1
-network-mode=2 # 0=FP32, 1=INT8, 2=FP16
-network-type=100 # >3 disables post-processing
-cluster-mode=4 # 1=DBSCAN 4=No Clustering
 gie-unique-id=3
-process-mode=2 # 1=Primary, 2=Secondary
-output-tensor-meta=1
-maintain-aspect-ratio=0
-tensor-meta-pool-size=200
 
 operate-on-class-ids=0;
+operated-on-class-name=choice
 
 [custom]
 labels=$CLASSES
